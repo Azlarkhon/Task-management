15b7a4a61673626227b3b5c72635370fefcc068b
Azlarkhon <xasanxojaevazlarxon2004@gmail.com>
2025-01-22 11:36:42 +0500

    commit

--- middleware/corsMiddleware.go
+++ middleware/corsMiddleware.go
@@ -5,13 +5,13 @@ import (
 )
 
 func CorsMiddleware(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
 	c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
 	c.Header("Access-Control-Allow-Credentials", "true")
 
 	if c.Request.Method == "OPTIONS" {
-		c.AbortWithStatus(200)
+		c.AbortWithStatus(204)
 		return
 	}
 
